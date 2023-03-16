a8bce13451f8d85377aa3079811768508fa6a14c
Heqing Huang <feipenghhq@gmail.com>
2023-03-16 00:04:53 -0700

    lfsr: designed parallel lfsr
        - Write a verilog file that calculate parallel LFSR with
          generic parameter
        - Added Yosys synthesis flow and a std cell library
        - Write a python script to generate the xor equation for parallel
          LFSR

--- README.md
+++ README.md
@@ -1,34 +1,50 @@
-# Advanced Chip Design Example
+# Verilog Design Example
 
 [TOC]
 
 ## Introduction
 
-This repository is based on the book **Advanced Chip Design Practical Examples in Verilog**
+This repository contains various design blocks written in SystemVerilog.
 
-- Writing my own verilog code
-- Taking some notes for the book
-- My own studying of some of the topics
+The topic of the design blocks are chosen based on the book **Advanced Chip Design Practical Examples in Verilog** since I am reading the book right now.
+
+
+
+## Tools
+
+- **cocotb**: https://www.cocotb.org/
+
+  - testbench is written in cocotb.
+
+- **yosys**: https://github.com/YosysHQ/yosys
+
+  - Synthesis tools used to synthesis the design
+
+  - Standard cell library used in synthesis is downloaded here: <http://www.vlsitechnology.org/synopsys/vsclib013.lib>
+
+
 
 
 ## Topics
 
 ### Digital Design Building Blocks
 
-- LFSR
+- Linear Feedback Shift Register (LFSR)
 - Scrambler
   - Parallel LFSR generation
-- Error Correction code
+- Error Correction code (ECC)
   - Hamming Code
 
-
+- Cyclic Redundancy Check (CRC)
 
 ## Repo structure
 
 ```txt
 ├── doc				# some documents/notes/references for this topic
-├── rtl				# rtl code
-├── scripts			# contains software models of the algorithm or scripts to generate some rtl code
-└── tb				# testbench code
+├── rtl				# rtl collateral
+├── scripts		# contains software models of the algorithm or scripts to generate some rtl code
+├── syn       # synthesis collateral
+└── tb				# testbench collateral
+
 ```
 
---
+++ lfsr/tb/lfsr_galois_parallel/lfsr_galois_parallel_test.py
@@ -0,0 +1,53 @@
+# ------------------------------------------------------------------------------------------------
+# Copyright 2023 by Heqing Huang (feipenghhq@gamil.com)
+# ------------------------------------------------------------------------------------------------
+# Author: Heqing Huang
+# Date Created: 03/03/2023
+# ------------------------------------------------------------------------------------------------
+# Testbench for Galois LFSR
+# ------------------------------------------------------------------------------------------------
+
+import cocotb
+from cocotb.triggers import Timer, RisingEdge, FallingEdge
+from cocotb.clock import Clock
+
+def Galois_LFSR_6801(initVal, dire):
+    lfsr_reg = initVal
+    while 1:
+        yield lfsr_reg
+        if dire == "MSB":
+            msb = lfsr_reg & 0x8000
+            if msb:
+                lfsr_reg = ((lfsr_reg << 1) ^ 0x6801) & 0xffff
+            else:
+                lfsr_reg = (lfsr_reg << 1) | msb
+        if dire == "LSB":
+            lsb = lfsr_reg & 0x1
+            if lsb:
+                lfsr_reg = (lfsr_reg >> 1) ^ (0x6801 >> 1)
+            else:
+                lfsr_reg = (lfsr_reg >> 1) | (lsb << 15)
+
+async def setup(dut):
+    dut.lfsr_in.value = 0
+    dut.data.value = 0
+
+async def tester(dut, dire, lfsr_out):
+    """Try accessing the design."""
+    lfsr_model = Galois_LFSR_6801(0xACE1, dire)
+    await setup(dut)
+    # get the expected value
+    for _ in range(17):
+        expected = next(lfsr_model)
+    # drive input to lfsr
+    dut.lfsr_in.value = 0xACE1
+    dut.data.value = 0
+    # wait some time
+    await Timer(10, "ns")
+    dut._log.info("LFSR output is %x. LFSR Model output is %x", lfsr_out.value, expected)
+    assert lfsr_out.value == expected, f"Got wrong value. See the value above."
+
+
+@cocotb.test()
+async def test_lsb(dut):
+    await tester(dut, "MSB", dut.lfsr_out)
